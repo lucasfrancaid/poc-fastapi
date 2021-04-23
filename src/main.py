2ab9679e66846ef572d45e5d7e3373dff561cd0b
Lucas França <lucasfrancaid@gmail.com>
2021-04-23 01:52:42 -0300

    Remove root route

--- src/main.py
+++ src/main.py
@@ -6,8 +6,3 @@ app = FastAPI()
 
 app.include_router(auth.router, tags=['Auth'], prefix='/auth')
 app.include_router(users.router, tags=['Users'], prefix='/users')
-
-@app.get('/')
-async def root():
-    return {'message': 'Hello World, it is FastAPI!'}
-
--- tests/test_routes.py
+++ tests/test_routes.py
@@ -6,17 +6,12 @@ from src.main import app
 client = TestClient(app)
 
 
-def test_main_root():
-    response = client.get('/')
-    assert response.status_code == 200
-    assert response.json() == {'message': 'Hello World, it is FastAPI!'}
-
-def test_main_route_auth_login():
+def test_route_auth_login():
     login = {'username': 'lucas', 'password': '123'}
     response = client.post('/auth/login', json=login)
     assert response.status_code == 200
 
-def test_main_route_users():
+def test_route_users():
     response = client.get('/users/')
     assert response.status_code == 200
 
